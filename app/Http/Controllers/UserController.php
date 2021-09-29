7f2816933fde216bd183b65f5d3f6ea8d4e7ba8c
Josh <joshsmetzger@gmail>
2021-09-29 17:12:32 +0000

    users section added to sidebar

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,13 @@ class UserController extends Controller
 {
     //
 
+    public function index(){
+
+        $users = User::all();
+
+        return view('admin.users.index', ['users'=>$users]);
+    }
+
     public function show(User $user){
         return view('admin.users.profile', ['user'=>$user]);
     }
--- resources/views/components/admin-master.blade.php
+++ resources/views/components/admin-master.blade.php
@@ -58,6 +58,8 @@
       <!-- Nav Item - Pages Collapse Menu -->
       <x-admin-sidebar-posts-links></x-admin-sidebar-posts-links>
 
+      <x-admin-sidebar-users-links></x-admin-sidebar-users-links>
+
       <!-- Nav Item - Utilities Collapse Menu -->
       <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
