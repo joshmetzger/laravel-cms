ab9335f1acab381d7b38ffb9135c3b486233fa61
Josh <joshsmetzger@gmail>
2021-09-30 21:44:00 +0000

    authentication for admin.users view

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,12 @@ Route::middleware('auth')->group(function(){
     Route::get('admin/users/{user}/profile', [App\Http\Controllers\UserController::class, 'show'])->name('user.profile.show');
     Route::put('admin/users/{user}/update', [App\Http\Controllers\UserController::class, 'update'])->name('user.profile.update');
     
-    Route::get('admin/users', [App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+    
     Route::delete('/admin/users/{user}/destroy', [App\Http\Controllers\UserController::class, 'destroy'])->name('user.destroy');
 
+});
+
+Route::middleware('role:admin')->group(function(){
+    
+    Route::get('admin/users', [App\Http\Controllers\UserController::class, 'index'])->name('users.index');
 });
