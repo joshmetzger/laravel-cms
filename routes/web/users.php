5eda85727ace39ddffe3c5aca9b453015a40d749
Josh <joshsmetzger@gmail>
2021-10-02 17:00:47 +0000

    web routes new directories. some issues with post index for old posts 100221

---
+++ routes/web/users.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::put('/users/{user}/update', [App\Http\Controllers\UserController::class, 'update'])->name('user.profile.update');
+Route::delete('/users/{user}/destroy', [App\Http\Controllers\UserController::class, 'destroy'])->name('user.destroy');
+
+
+Route::middleware(['role:admin','auth'])->group(function(){
+
+    Route::get('/users', [App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+});
+
+Route::middleware(['can:view,user'])->group(function(){
+
+    Route::get('/users/{user}/profile', [App\Http\Controllers\UserController::class, 'show'])->name('user.profile.show');
+
+});
